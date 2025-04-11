b883e9652bbdca362b5dbb71e6afc586e137e16e
z-d-a-y <fazetemori@gmail.com>
2025-04-11 08:43:49 -0700

    Update Decision.cpp

--- Decision.cpp
+++ Decision.cpp
@@ -4,33 +4,48 @@
 using namespace std;
 int main() {
     int userinput;
-    bool tester = false;
-    while(!tester) {
+    bool valid = false;
     cout << "Pick one" << endl << "1.words" << endl << "2.numbers" << endl << "ENTER: ";
     cin >> userinput;
 if (userinput == 1){
     char userinput;
-    cout << "Pick one" << endl << "a" << endl << "b" << endl << "c" << endl << "ENTER: ";
+    cout << "\nPick one" << endl << "a" << endl << "b" << endl << "c" << endl << "ENTER: ";
     cin >> userinput;
+    while(!valid) {
     switch(userinput){
-    case 'a':cout << "You have chosen word a.";break;
-    case 'b':cout << "You have chosen word b.";break;
-    case 'c':cout << "You have chosen word c.";break;
-    default: cout << "\nError.\nCheck before you click enter.";break;   
-    }}
-else if (userinput == 2){
-    int userinput;
-    cout << "Pick one" << endl << "1" << endl << "2" << endl << "3" << endl << "ENTER: ";
+    case 'a':cout << "You have chosen word a.";valid = true;break;
+    case 'b':cout << "You have chosen word b.";valid = true;break;
+    case 'c':cout << "You have chosen word c.";valid = true;break;
+    default: 
+    cout << "\nError. Check before you enter.\n";
+    cout << "\nPick one" << endl << "a" << endl << "b" << endl << "c" << endl << "ENTER: ";
     cin >> userinput;
-    switch(userinput) {
-    case 1:cout << "You have chosen number 1.";break;
-    case 2:cout << "You have chosen number 2.";break;
-    case 3:cout << "You have chosen number 3.";break;
-    default: cout << "\nError.\nCheck before you click enter.";break; 
-    }}
-    else {
-        printf("\nError. Try again.");
+    break;
     }
 }
+}
+else if (userinput == 2){
+    int userinputnum;
+    bool validnum = false;
+    cout << "\nPick one" << endl << "1" << endl << "2" << endl << "3" << endl << "ENTER: ";
+    cin >> userinputnum;
+    while(!validnum) {
+    switch(userinputnum) {
+    case 1:cout << "You have chosen number 1.";validnum = true;break;
+    case 2:cout << "You have chosen number 2.";validnum = true;break;
+    case 3:cout << "You have chosen number 3.";validnum = true;break;
+    default: 
+    cout << "\nError. Check before you enter.\n";
+    cout << "\nPick one" << endl << "1" << endl << "2" << endl << "3" << endl << "ENTER: ";
+    cin >> userinputnum;
+    break;
+    }
+}
+}
+    else {
+        printf("Error. Try again.\n");
+    }
+
     return 0;
+    
 }
